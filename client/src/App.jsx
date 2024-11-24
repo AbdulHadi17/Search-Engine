89df686896c492fef01777bbb1260e19b5b7d734
AbdulHadi17 <hadisaqib2048@gmail.com>
2024-11-24 19:24:36 +0500

    Initialized Vite + Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center bg-slate-900 '>
+
+    </div>
+  )
+}
+
+export default App
