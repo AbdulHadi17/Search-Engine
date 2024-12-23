60ab0bb5f9c6cd02e5f01ee6d956c7589ea55e49
Anas Rehman <anasrehman464092@gmail.com>
2024-12-23 18:27:36 +0500

    add AddingDoc component, utility functions, and update configuration files  adding shadcn

---
+++ client/src/AddingDoc.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddingDoc() {
+    return <div>AddingDoc</div>;
+}
+
+export default AddingDoc;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import Home from './Home';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import Home from "./Home";
+import { Route, Routes } from "react-router-dom";
+import AddingDoc from "./AddingDoc";
 // import Results from './Results';
 
 const App = () => {
-  return (
-<>
-<Routes>
-  <Route exact path='/' element={<Home/>}/>
-  {/* <Route path='/results' element={<Results/>}/> */}
-</Routes>
-</>  
-
-);
+    return (
+        <>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/add-doc" element={<AddingDoc />} />
+                {/* <Route path='/results' element={<Results/>}/> */}
+            </Routes>
+        </>
+    );
 };
 
 export default App;
