147241b75ad6b109b1110b6fc4ebe6156b229eb9
Anas Rehman <anasrehman464092@gmail.com>
2024-12-25 19:05:09 +0500

    feat: input processing to generate lexicon, forward index, inverted index, and update barrels

--- server/main.py
+++ server/main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import JSONResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware  # Import CORSMiddleware
-import pandas as pd
 from pathlib import Path
-import os
 import sys
-
+import pathlib
 # Add the parent directory of 'server' to the Python path
 sys.path.append(str(Path(__file__).resolve().parent))
-
-from Preprocessing.lexicon import generate_lexicon  # Corrected import
 
 app = FastAPI()
 
@@ -34,18 +30,34 @@ async def process_csv(file: UploadFile):
             f.write(file.file.read())
 
         # Generate the lexicon
-        generate_lexicon(temp_file)
+        output_path = (pathlib.Path().absolute() / "Preprocessing" /"lexicon.csv")
+        from Preprocessing.LexiconGenerator import LexiconGenerator
+        lex_gen = LexiconGenerator(output_path)
+        lex_gen.generate(temp_file)
 
-        # Delete the temporary file after success
-        if temp_file.exists():
-            temp_file.unlink()  # Deletes the file
+        # Generate the forward index
+        forward_index_path = (pathlib.Path().absolute() / "Forward_Index" / "forward_index.json")
+        new_forward_index_path = (pathlib.Path().absolute() / "Forward_Index" / "New_forward_index.json")
+        from Forward_Index.ForwardIndexGenerator import ForwardIndexGenerator
+        generator = ForwardIndexGenerator(temp_file, output_path, forward_index_path, new_forward_index_path)
+        generator.generate_forward_index()
+
+        # Generate the inverted index
+        new_output_file_path = (pathlib.Path().absolute() / "inverted_index" / "New_Inverted.json")
+        from inverted_index.InvertedIndexGenerator import InvertedIndexGenerator
+        generator = InvertedIndexGenerator(new_forward_index_path, new_output_file_path)
+        generator.generate()
+
+        # Generate the barrels
+        output_dir = (pathlib.Path().absolute() / "inverted_index" / "barrels")
+        from inverted_index.BarrelManager import BarrelManager
+        manager = BarrelManager(output_dir)
+        manager.update_barrels_with_json(new_output_file_path)
 
         return JSONResponse(
-            content={"message": "Lexicon generated successfully"},
+            content={"message": "Successfully processed the document."},
             status_code=200,
         )
     except Exception as e:
-        # Ensure temp file is deleted in case of failure
-        if temp_file.exists():
-            temp_file.unlink()
+        print(e)
         return JSONResponse(content={"error": str(e)}, status_code=500)
